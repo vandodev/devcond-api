135afe213bd81f04462f3ffae1285a781d70b656
Evandro oliveira <vandoaparecido@hotmail.com>
2022-03-05 00:32:36 -0300

    :fork_and_knife: Visualizando avisos

--- app/Http/Controllers/WarningController.php
+++ app/Http/Controllers/WarningController.php
@@ -3,8 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Storage;
+// use Illuminate\Support\Facades\Validator;
+
+use App\Models\Unit;
+use App\Models\Warning;
 
 class WarningController extends Controller
 {
-    //
+   public function getMyWarnings(Request $request) {
+        $array = ['error' => ''];
+
+        $property = $request->input('property');
+        if($property) {
+            $user = Auth()->user();
+
+            $unit = Unit::where('id', $property)
+            ->where('id_owner', $user['id'])
+            ->count();
+
+            if($unit > 0) {
+              $warnings = Warning::where('id_unit', $property)
+                ->orderBy('datecreated', 'DESC')
+                ->orderBy('id', 'DESC')
+                ->get();
+
+                $array['list'] = $warnings;
+            } else {
+                $array['error'] = 'Esta unidade não é sua!';
+            }
+
+        } else {
+            $array['error'] = 'A propriedade é necessária!';
+        }
+
+        return $array;
+    }
 }
