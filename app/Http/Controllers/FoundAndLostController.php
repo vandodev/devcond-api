0976a3f29761be1804ce20052af0a977513708aa
Evandro oliveira <vandoaparecido@hotmail.com>
2022-03-08 00:01:20 -0300

    :fork_and_knife: Listando objetos perdidos

--- app/Http/Controllers/FoundAndLostController.php
+++ app/Http/Controllers/FoundAndLostController.php
@@ -1,10 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\FoundAndLost;
 use Illuminate\Http\Request;
 
 class FoundAndLostController extends Controller
 {
-    //
+    public function getAll() {
+        $array = ['error' => ''];
+
+        $lost = FoundAndLost::where('status', 'LOST')
+        ->orderBy('datecreated', 'DESC')
+        ->orderBy('id', 'DESC')
+        ->get();
+
+        $array['lost'] = $lost;
+
+        return $array;
+    }
 }
