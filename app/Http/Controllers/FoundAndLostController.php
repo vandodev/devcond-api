92246a49c8081be6a6f9f0c129891b8915fb87a1
Evandro oliveira <vandoaparecido@hotmail.com>
2022-03-08 00:42:38 -0300

    :fork_and_knife: Exibindo itens encontrados com data e caminho da imagem formatados

--- app/Http/Controllers/FoundAndLostController.php
+++ app/Http/Controllers/FoundAndLostController.php
@@ -14,12 +14,24 @@ class FoundAndLostController extends Controller
         ->orderBy('id', 'DESC')
         ->get();
 
+         $recovered = FoundAndLost::where('status', 'RECOVERED')
+        ->orderBy('datecreated', 'DESC')
+        ->orderBy('id', 'DESC')
+        ->get();
+
+
         foreach($lost as $lostkey => $lostValue) {
             $lost[$lostkey]['datecreated'] = date('d/m/Y', strtotime($lostValue['datecreated']));
             $lost[$lostkey]['photo'] = asset('storage/'.$lostValue['photo']);
         }
 
+        foreach($recovered as $recKey => $recValue) {
+            $recovered[$recKey]['datecreated'] = date('d/m/Y', strtotime($recValue['datecreated']));
+            $recovered[$recKey]['photo'] = asset('storage/'.$recValue['photo']);
+        }
+
         $array['lost'] = $lost;
+        $array['recovered'] = $recovered;
 
         return $array;
     }
