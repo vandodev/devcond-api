1dba6cec9c79116ff868dcd6cd58c4ca33a952d7
Evandro oliveira <vandoaparecido@hotmail.com>
2022-03-12 00:02:51 -0300

    :fork_and_knife: Exibindo reservas disponíveis

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Area;
+
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    //
+    public function getReservations() {
+        $array = ['error' => ''];
+        $areas = Area::where('allowed', 1)->get();
+        $array['list'] = $areas;
+        return $array;
+    }
 }
