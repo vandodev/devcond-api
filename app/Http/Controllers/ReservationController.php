bb64d88903cc532167a70a75a7fa7648e2aefb6f
Evandro oliveira <vandoaparecido@hotmail.com>
2022-03-15 01:01:26 -0300

    :fork_and_knife: Verificar se está dentro da Disponibilidade Padrão

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Area;
 use App\Models\AreaDisabledDay;
 use App\Models\Reservation;
+use App\Models\Unit;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -203,5 +204,61 @@ class ReservationController extends Controller
 
         return $array;
      }
+
+      public function setReservation($id, Request $request) {
+          $array = ['error' => ''];
+
+            $validator = Validator::make($request->all(), [
+             'date' => 'required|date_format:Y-m-d',
+             'time' => 'required|date_format:H:i:s',
+             'property' => 'required'
+            ]);
+
+            if(!$validator->fails()) {
+
+                $date = $request->input('date');
+                $time = $request->input('time');
+                $property = $request->input('property');
+
+                $unit = Unit::find($property);
+                $area = Area::find($id);
+
+                if($unit && $area) {
+                    $can = true;
+
+                    $weekday = date('w', strtotime($date));
+                    // Verificar se está dentro da Disponibilidade Padrão
+                    $allowedDays = explode(',', $area['days']);
+
+                    if(!in_array($weekday, $allowedDays)) {
+                        $can = false;
+                    } else {
+                        $start = strtotime($area['start_time']);
+                        $end = strtotime('-1 hour', strtotime($area['end_time']));
+                        $revtime = strtotime($time);
+                    if($revtime < $start || $revtime > $end) {
+                        $can = false;
+                    }
+                }
+
+                if($can){
+
+                }else{
+                    $array['error'] = 'Reserva não permitida neste dia/horário';
+                    return $array;
+                }
+
+                }else{
+                    $array['error'] = 'Dados incorretos!';
+                    return $array;
+                }
+
+            }else{
+                $array['error'] = $validator->errors()->first();
+                return $array;
+            }
+
+          return $array;
+      }
 
 }
